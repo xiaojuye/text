76e1b5b26226489b4709005553836d18498a4082
李驹 <986008610@qq.com>
2017-02-26 15:16:39 +0800

    初始化

---
+++ text1/text1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text1
+//
+//  Created by 李驹 on 17/2/26.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text1/text1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text1
+//
+//  Created by 李驹 on 17/2/26.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
